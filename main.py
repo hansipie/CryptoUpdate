b73e190767ae56e776342ce25005a8ba211b8a18
Hansi P <hansipie@users.noreply.github.com>
2023-12-17 16:44:01 +0200

    fix docker

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
 from dotenv import load_dotenv
 from modules import Exporter, Updater
 
 if __name__ == "__main__":
     load_dotenv()
+
+    if os.getenv('MY_COINMARKETCAP_APIKEY') is None:
+        print("Please set your Coinmarketcap API key in the .env file")
+        quit()
+    if os.getenv('NOTION_API_TOKEN') is None:
+        print("Please set your Notion API key in the .env file")
+        quit()
 
     #update database with current market values
     Updater.Updater().UpdateCrypto()
