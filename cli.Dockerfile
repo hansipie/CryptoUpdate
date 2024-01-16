f727b2e7a83dbaf895ced1b94289138a071162e8
Hansi P <hansipie@users.noreply.github.com>
2024-01-16 13:02:47 +0200

    cli tools

--- cli.Dockerfile
+++ cli.Dockerfile
@@ -30,4 +30,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Définissez la commande pour exécuter votre application
-CMD streamlit run 0_Home.py --server.port 8080
+CMD python updateNotion.py
--- updateNotion_cli.py
+++ updateNotion_cli.py
@@ -1,19 +1,38 @@
 import os
 import configparser
-from modules import Exporter, Updater
+from modules.Exporter import Exporter
+from modules.Notion import Notion
+from modules.Updater import Updater
 
 if __name__ == "__main__":
-    #read config
+
     config = configparser.ConfigParser()
     config.read('./data/settings.ini')
 
+    try:
+        #read config
+        notion_api_token = config["DEFAULT"]["notion_token"]
+        coinmarketcap_api_token = config["DEFAULT"]["coinmarketcap_token"]
+        database = config["Notion"]["database"]
+        parent_page = config["Notion"]["parent_page"]
 
+    except KeyError as ke:
+        print("Error: " + type(ke).__name__ + " - " + str(ke))
+        print("Please set your settings in the settings file")
+        quit()
+
+    notion = Notion(notion_api_token)
+    db_id = notion.getObjectId(database, "database", parent_page)
+    if db_id == None:
+        print("Error: Database not found")
+        quit()
+            
     #update database with current market values
-    Updater.Updater(config["DEFAULT"]["coinmarketcap_token"], config["DEFAULT"]["notion_token"]).UpdateCrypto()
+    Updater(coinmarketcap_api_token, notion_api_token, db_id).UpdateCrypto()
 
     #export database to csv file. 
     # destination: ./archives/[epoch]/*.csv
-    csvfile = Exporter.Exporter(config["DEFAULT"]["notion_token"]).GetCSVfile()
+    csvfile = Exporter(notion_api_token).GetCSVfile(database)
     print("Output file: ", csvfile)
-
     print("Done.")
+
