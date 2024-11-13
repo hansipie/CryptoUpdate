205ec92d40d00fd65485213f3883f22aa12e24eb
Hansi P. <hansipie@users.noreply.github.com>
2024-11-13 23:01:30 +0200

    almost there

--- app_pages/2_Import.py
+++ app_pages/2_Import.py
@@ -6,11 +6,10 @@ import os
 import configparser
 import logging
 import pandas as pd
-from modules import walletVision
+from modules import aiprocessing
 from PIL import Image
 
 logger = logging.getLogger(__name__)
-
 
 def decodeImg(image_bytes) -> bytes:
     image_file = io.BytesIO(image_bytes)
@@ -21,74 +20,70 @@ def decodeImg(image_bytes) -> bytes:
         img.save(img_bytes, format="PNG")
     return img_bytes.getvalue()
 
+def extract_gui(input: any):
+    submit_button = st.form_submit_button(
+        label="Extract data",
+        help="Extract data from the image.",
+        use_container_width=True,
+    )
+    if submit_button:
+        with st.spinner("Extracting data..."):
+            try:
+                if isinstance(input, bytes):
+                    message_json, _ = aiprocessing.extract_from_img(
+                        input, config["DEFAULT"]["openai_token"]
+                    )
+                    st.session_state.import_output = pd.DataFrame.from_dict(loads(message_json).get("assets"))
+                elif isinstance(input, pd.DataFrame):
+                    message_json, _ = aiprocessing.extract_from_df(
+                        input, config["DEFAULT"]["openai_token"]
+                    )
+                    st.session_state.import_output = pd.DataFrame.from_dict(loads(message_json).get("assets"))
+                else:
+                    raise ValueError("Invalid input type")
+            except KeyError as ke:
+                st.error("Error: " + type(ke).__name__ + " - " + str(ke))
+                st.error("Please set your settings in the settings page")
+                traceback.print_exc()
+                st.stop()
+            except Exception as e:
+                st.error("Error: " + str(e))
+                traceback.print_exc()
+                st.stop()
+        
+        st.dataframe(st.session_state.import_output, use_container_width=True)
+        st.balloons()
+
 
 def processImg(file):
     imagefile = decodeImg(file.getvalue())
-    with st.form(key="my_form"):
+    st.session_state.import_input = imagefile
+    st.session_state.import_output = None
+
+    with st.form(key="extract_form"):
         col_img, col_json = st.columns(2)
         with col_img:
             st.image(imagefile)
         with col_json:
-            submit_button = st.form_submit_button(
-                label="Extract data",
-                help="Extract data from the image.",
-                use_container_width=True,
-            )
-            if submit_button:
-                if debugflag:
-                    message_json = {
-                        "Best Blockchains": "€ 2,130.18",
-                        "Solana": {"value": "€ 1,991.66", "amount": "10.59116226 SOL"},
-                        "Bitcoin": {"value": "€ 1,802.29", "amount": "0.02522609 BTC"},
-                        "USD Coin": {"value": "€ 997.42", "amount": "1,068.6508 USDC"},
-                        "Ethereum": {"value": "€ 979.92", "amount": "0.3468965 ETH"},
-                        "SwissBorg": {
-                            "value": "€ 411.99",
-                            "amount": "2,262.907403 BORG",
-                        },
-                        "Golden": "€ 344.89",
-                        "Xborg": {"value": "€ 262.94", "amount": "1,096.309539 XBG"},
-                    }
-                else:
-                    with st.spinner("Extracting data..."):
-                        try:
-                            message_json, tokens = walletVision.extract_crypto(
-                                imagefile, config["DEFAULT"]["openai_token"]
-                            )
-                        except KeyError as ke:
-                            st.error("Error: " + type(ke).__name__ + " - " + str(ke))
-                            st.error("Please set your settings in the settings page")
-                            traceback.print_exc()
-                            st.stop()
-                        except Exception as e:
-                            st.error("Error: " + str(e))
-                            traceback.print_exc()
-                            st.stop()
-                
-                st.json(message_json)
-                st.balloons()
-
+            extract_gui(imagefile)
 
 def processCSV(file) -> str:
-    df = pd.read_csv(file)
+    st.session_state.import_input = pd.read_csv(file)
+    
+    with st.form(key="extract_form"):
+        col_input, col_output = st.columns(2)
+        with col_input:
+            st.dataframe(st.session_state.import_input, use_container_width=True)
+        with col_output:
+            extract_gui(st.session_state.import_input)
 
-    with st.form(key="my_form"):
-        col_data, col_json = st.columns(2)
-        with col_data:
-            st.data_editor(df, hide_index=True, use_container_width=True)
-        with col_json:
-            submit_button = st.form_submit_button(
-                label="Extract data",
-                help="Extract data from the dataframe.",
-                use_container_width=True,
-            )
-            if submit_button:
-                with st.spinner("Extracting data..."):
-                    message_json = df.to_json(orient="records")
-
-                st.json(message_json)
-                st.balloons()
-
+# session state variable
+if "import_input" not in st.session_state:
+    st.session_state.import_input = None
+if "import_output" not in st.session_state:
+    st.session_state.import_output = None
+if "import_type" not in st.session_state:
+    st.session_state.import_type = None
 
 configfilepath = "./data/settings.ini"
 if not os.path.exists(configfilepath):
@@ -109,9 +104,34 @@ file = st.file_uploader("Upload a file", type=["png", "jpg", "jpeg", "csv"])
 
 if file is not None:
     logger.debug(f"File: {file} - file type: {file.type}")
-    if file.type == "application/vnd.ms-excel":
-        st.write("CSV file detected")
+    st.session_state.import_type = file.type
+
+    if st.session_state.import_type == "application/vnd.ms-excel":
+        logger.debug("CSV file detected")
         processCSV(file)
     else:
-        st.write("Image file detected")
+        logger.debug("Image file detected")
         processImg(file)
+
+elif st.session_state.import_input is not None:
+
+    with st.form(key="extract_form"):
+        col_input, col_output = st.columns(2)
+        with col_input:
+            if st.session_state.import_type == "application/vnd.ms-excel":
+                st.dataframe(st.session_state.import_input, use_container_width=True)
+            else:
+                st.image(st.session_state.import_input)
+        with col_output:
+            extract_gui(st.session_state.import_input)
+
+
+
+
+# Display session state variables
+with st.expander("type:"):
+    st.write(st.session_state.import_type)
+with st.expander("Input:"):
+    st.write(st.session_state.import_input)
+with st.expander("Output:"):
+    st.write(st.session_state.import_output)
---
+++ modules/aiprocessing.py
@@ -0,0 +1,105 @@
+import base64
+import pandas as pd
+import io
+import traceback
+import logging
+import pandas as pd
+from openai import OpenAI
+from PIL import Image
+
+logger = logging.getLogger(__name__)
+
+def get_image_type(image: bytes):
+    try:
+        image_file = io.BytesIO(image)
+        with Image.open(image_file) as img:
+            return img.format.lower()
+    except IOError as e:
+        logger.debug(e)
+        return None
+
+def extract_from_df(df: pd.DataFrame, api_key: str):
+    messages = [
+        {"role": "system", "content": "You are a data extraction model."},
+        {"role": "assistant", "content": f"data: ```{df.to_json()}```"},
+        {
+            "role": "user",
+            "content": (
+                        "The data is a JSON dump of a cryptocurrency portfolio.\n"
+                        "Analyse it and identifying individual assets details.\n"
+                        "Extract the following informations:\n"
+                        "- Asset name\n"
+                        "- Asset amount with unit (if applicable)\n"
+                        "- Asset fiat value (€)(if applicable)\n"
+                        "Format the extracted data into a well-structured JSON format, ensuring that each asset is represented as an object within an array.\n"
+                        "If you can not find cryptocurrencies data in the image, return only the string \"NO_DATA\"."
+                    ),
+        }
+    ]
+    return call_ai(messages, api_key)
+
+def extract_from_img(bytes_data: bytes, api_key:str):
+
+    type_image = get_image_type(bytes_data)
+    if type_image is None:
+        logger.debug("Invalid image.")
+        return None, None
+    else:
+        logger.debug(f"Image type: {type_image}")
+
+    base64_image = base64.b64encode(bytes_data).decode("utf-8")
+
+    messages = [
+        {"role": "system", "content": "You are a data extraction model."},
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": (
+                        "The image is a screenshot of a cryptocurrency portfolio.\n"
+                        "Analyse it and identifying individual assets details.\n"
+                        "Extract the following informations:\n"
+                        "- Asset name\n"
+                        "- Asset amount with unit (if applicable)\n"
+                        "- Asset fiat value (€)(if applicable)\n"
+                        "Format the extracted data into a well-structured JSON format, ensuring that each asset is represented as an object within an array.\n"
+                        "If you can not find cryptocurrencies data in the image, return only the string \"NO_DATA\"."
+                    ),
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/{type_image};base64,{base64_image}"
+                    },
+                },
+            ],
+        },
+    ]
+    return call_ai(messages, api_key)
+
+def call_ai(messages: list, api_key: str):
+    # Create an OpenAI object
+    model = OpenAI(api_key=api_key)
+    total_tokens = 0
+
+    try:
+        logger.debug("Processing ...")
+        response = model.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages,
+            response_format={"type": "json_object"},
+        )
+    except Exception as e:
+        traceback.print_exc()
+        return None, None
+
+    message = response.choices[0].message
+    total_tokens = response.usage.total_tokens
+    logger.debug(f"Number of tokens used: {total_tokens}")
+
+    if "NO_DATA" in message.content:
+        logger.debug("Invalid image.")
+        return None, total_tokens
+
+    return message.content, total_tokens
